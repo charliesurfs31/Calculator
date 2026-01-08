953ed3660f72890928aa0874cf822866e3acaea7
Charlie Gainey <cgaineyh@gmail.com>
2026-01-07 22:10:10 -0800

    to dos

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const sum = function(a, b) {
 button.addEventListner('click', function) {
 
 }
+
+/* TO DO:
+1. Add functionality for decimal point button (single use).
+2. Add functionality for clear button.
+3. reduce = to single use.
+4. Add keyboard support.
+5. Style digits.
+*/
