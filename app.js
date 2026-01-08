5b026f3605b81da00240c1edcd875d4ef091a296
Charlie Gainey <cgaineyh@gmail.com>
2026-01-08 11:45:48 -0800

    limit to nine digits and one decimal use

--- app.js
+++ app.js
@@ -1,17 +1,76 @@
-const sum = function(a, b) {
-    const sumOne = a + b;
-    return sumOne;
+let firstNumber = '';
+let operator = '';
+let isSecondNumber = false;
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
 }
 
-
-button.addEventListner('click', function) {
-
+function subtract(a, b) {
+    const difference = a - b;
+    return difference;
 }
 
-/* TO DO:
-1. Add functionality for decimal point button (single use).
-2. Add functionality for clear button.
-3. reduce = to single use.
-4. Add keyboard support.
-5. Style digits.
-*/
+function multiply(a, b) {
+    const product = a * b;
+    return product;
+}
+
+function divide(a, b) {
+    const quotient = a / b;
+    return quotient;
+}
+
+const numButtons = document.querySelectorAll('.frontButtons #num');
+for (let i=0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click', () => {
+        const digit = numButtons[i].textContent;
+        const screen = document.querySelector('.screen');
+        if(isSecondNumber) {
+            screen.textContent = '';
+            isSecondNumber = false;
+        }
+        if((digit === '.') && (screen.textContent.includes('.'))) {
+        }
+        if(screen.textContent.length >= 9) {
+            
+        }
+        else{
+        screen.innerHTML += digit;
+        }
+    })
+}
+
+const operButtons = document.querySelectorAll('.frontButtons #oper');
+for (let i=0; i < operButtons.length; i++) {
+    operButtons[i].addEventListener('click', () => {
+        const screen = document.querySelector('.screen');
+        if(operButtons[i].textContent === '=') {
+            const secondNumber = screen.textContent;
+            if(operator === '+') {
+                const result = add(Number(firstNumber), Number(secondNumber));
+                screen.textContent = result;
+            }
+            if(operator === '−') {
+                const result = subtract(Number(firstNumber), Number(secondNumber));
+                screen.textContent = result;
+            }
+            if(operator === '×') {
+                const result = multiply(Number(firstNumber), Number(secondNumber));
+                screen.textContent = result;
+            }
+            if(operator === '÷') {
+                const result = divide(Number(firstNumber), Number(secondNumber));
+                screen.textContent = result;
+            }
+        }
+        else {
+            firstNumber = screen.textContent;
+            operator = operButtons[i].textContent;
+            isSecondNumber = true;
+        }
+
+        }
+    )
+}
